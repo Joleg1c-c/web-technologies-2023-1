9a0f75fb16371f05111b2b2a73b25d61ae652e56
Joleg1c-c <Joleg1c-c@mail.ru>
2024-01-08 20:45:12 +0500

    показ чей ход

--- components/TicTacToe.js
+++ components/TicTacToe.js
@@ -196,6 +196,9 @@ export const TicTacToe = {
    */
   changeTurnValue() {
     this.isXTurn = !this.isXTurn;
+    setTimeout(() => {
+      alert('Ход ' + this.getCurrentTurnValue())
+    })
   },
 
   /**
